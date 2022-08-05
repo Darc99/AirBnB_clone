e33561d4a0ee4b1c48ed32eaec0035e6b2221d10
Darc99 <lennyyhugoh@gmail.com>
2022-08-05 23:39:46 +0100

    create base model from dict(4)

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,22 @@ class BaseModel:
 
     """
 
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """
         Init Method
 
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """
