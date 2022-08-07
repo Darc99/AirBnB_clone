dfc300f32ab219b7fc6cd3ed48c9c3f86cea838d
Darc99 <lennyyhugoh@gmail.com>
2022-08-07 03:34:15 +0100

    init console

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    
+    """
+
+    def do_quit(self, line):
+        """
+        
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        
+        """
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
