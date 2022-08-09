c66e86914acaee92851b6d5e1f066fa054d4e18a
Darc99 <lennyyhugoh@gmail.com>
2022-08-09 04:00:14 +0100

    console adj

--- console.py
+++ console.py
@@ -1,57 +1,24 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
+""" Console """
 import cmd
-import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-
-
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-
-    Attributes:
-        prompt (str): The command prompt.
-    """
-
-    prompt = "(hbnb) "
-
-    def emptyline(self):
-        """Do nothing upon receiving an empty line."""
-        pass
+    """ General Class for HBNBCommand """
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
-        return True
+        """ Exit method for quit typing """
+        exit()
 
     def do_EOF(self, arg):
-        """EOF signal to exit the program."""
-        print("")
-        return True
+        """ Exit method for EOF """
+        print('')
+        exit()
 
-if __name__ == "__main__":
+    def emptyline(self):
+        """ Method to pass when emptyline entered """
+        pass
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
